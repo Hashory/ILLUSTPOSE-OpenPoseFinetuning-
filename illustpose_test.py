294b9985f0069f76426c809c4b59b738d3b4bb79
Hashory <89839285+Hashory@users.noreply.github.com>
2025-02-13 16:31:48 +0900

    Refactor: Add dependences for pyproject.toml and formatting code.

--- illustpose_test.py
+++ illustpose_test.py
@@ -1,11 +1,9 @@
-from PIL import Image
 import cv2
-import numpy as np
-from matplotlib import cm
 import matplotlib.pyplot as plt
-
+import numpy as np
 import torch
 
+from utils.decode_pose import decode_pose
 from utils.openpose_net import OpenPoseNet
 
 # 学習済みモデルと本章のモデルでネットワークの層の名前が違うので、対応させてロードする
@@ -13,8 +11,7 @@ from utils.openpose_net import OpenPoseNet
 net = OpenPoseNet()
 
 # 学習済みパラメータをロードする
-net_weights = torch.load(
-    './weights/ILLUST100.pth', map_location={'cuda:0': 'cpu'})
+net_weights = torch.load("./weights/ILLUST100.pth", map_location={"cuda:0": "cpu"})
 keys = list(net_weights.keys())
 
 weights_load = {}
@@ -22,19 +19,18 @@ weights_load = {}
 # ロードした内容を、本書で構築したモデルの
 # パラメータ名net.state_dict().keys()にコピーする
 for i in range(len(keys)):
-    weights_load[list(net.state_dict().keys())[i]
-                 ] = net_weights[list(keys)[i]]
+	weights_load[list(net.state_dict().keys())[i]] = net_weights[list(keys)[i]]
 
 # コピーした内容をモデルに与える
 state = net.state_dict()
 state.update(weights_load)
 net.load_state_dict(state)
 
-print('ネットワーク設定完了：学習済みの重みをロードしました')
+print("ネットワーク設定完了：学習済みの重みをロードしました")
 
 # 草野球の画像を読み込み、前処理します
 
-test_image = './data/test1.jpg'
+test_image = "./data/test1.jpg"
 oriImg = cv2.imread(test_image)  # B,G,Rの順番
 
 # BGRをRGBにして表示
@@ -45,7 +41,7 @@ size = (368, 368)
 img = cv2.resize(oriImg, size, interpolation=cv2.INTER_CUBIC)
 
 # 画像の前処理
-img = img.astype(np.float32) / 255.
+img = img.astype(np.float32) / 255.0
 
 # 色情報の標準化
 color_mean = [0.485, 0.456, 0.406]
@@ -54,8 +50,8 @@ color_std = [0.229, 0.224, 0.225]
 preprocessed_img = img.copy()[:, :, ::-1]  # BGR→RGB
 
 for i in range(3):
-    preprocessed_img[:, :, i] = preprocessed_img[:, :, i] - color_mean[i]
-    preprocessed_img[:, :, i] = preprocessed_img[:, :, i] / color_std[i]
+	preprocessed_img[:, :, i] = preprocessed_img[:, :, i] - color_mean[i]
+	preprocessed_img[:, :, i] = preprocessed_img[:, :, i] / color_std[i]
 
 # （高さ、幅、色）→（色、高さ、幅）
 img = preprocessed_img.transpose((2, 0, 1)).astype(np.float32)
@@ -78,11 +74,13 @@ pafs = cv2.resize(pafs, size, interpolation=cv2.INTER_CUBIC)
 heatmaps = cv2.resize(heatmaps, size, interpolation=cv2.INTER_CUBIC)
 
 pafs = cv2.resize(
-    pafs, (oriImg.shape[1], oriImg.shape[0]), interpolation=cv2.INTER_CUBIC)
+	pafs, (oriImg.shape[1], oriImg.shape[0]), interpolation=cv2.INTER_CUBIC
+)
 heatmaps = cv2.resize(
-    heatmaps, (oriImg.shape[1], oriImg.shape[0]), interpolation=cv2.INTER_CUBIC)
+	heatmaps, (oriImg.shape[1], oriImg.shape[0]), interpolation=cv2.INTER_CUBIC
+)
 
-from utils.decode_pose import decode_pose
+
 _, result_img, _, _ = decode_pose(oriImg, heatmaps, pafs)
 
 # 結果を描画
